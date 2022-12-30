d0cb2a237f85ab574429ccd02f75ec45cece9494
Joseph Ledo-Massey <104868843+Joeyy16@users.noreply.github.com>
2022-12-30 14:30:02 -0700

    Update main.py

--- Data Analysis with Python/Mean-Variance-Standard Deviation Calculator/main.py
+++ Data Analysis with Python/Mean-Variance-Standard Deviation Calculator/main.py
@@ -1,8 +1,10 @@
+"""
+This code is printing the result of the calculate() function when it is passed a list of integers as an argument. The calculate() function is defined in the mean_var_std module and it is used to calculate the mean, variance, and standard deviation of a list of numbers. When this code is executed, it will print a tuple containing the mean, variance, and standard deviation of the list of integers passed to the calculate() function.
+
+"""
+
 # This entrypoint file to be used in development. Start by reading README.md
 import mean_var_std
 from unittest import main
 
 print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
-
-# Run unit tests automatically
-main(module='test_module', exit=False)
