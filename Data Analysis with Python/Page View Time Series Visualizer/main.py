dc43c6d3e42f19919c3c05a109e345cc102859c5
Joseph Ledo-Massey <104868843+Joeyy16@users.noreply.github.com>
2022-12-30 15:35:56 -0700

    Update main.py

--- Data Analysis with Python/Page View Time Series Visualizer/main.py
+++ Data Analysis with Python/Page View Time Series Visualizer/main.py
@@ -1,11 +1,14 @@
+"""
+The code above is calling three functions from the time_series_visualizer module. These functions are draw_line_plot(), draw_bar_plot(), and draw_box_plot(). These functions are expected to create and save plots using time series data. The first function, draw_line_plot(), creates and saves a line plot. The second function, draw_bar_plot(), creates and saves a bar plot. The third function, draw_box_plot(), creates and saves a box plot. It is not possible to provide more information about these functions without seeing their code.
+"""
+
 # This entrypoint file to be used in development. Start by reading README.md
 import time_series_visualizer
-from unittest import main
+
 
 # Test your function by calling it here
 time_series_visualizer.draw_line_plot()
 time_series_visualizer.draw_bar_plot()
 time_series_visualizer.draw_box_plot()
 
-# Run unit tests automatically
-main(module='test_module', exit=False)
+
