3c74de7c7c02d372f864781c07c9fb9c27dc2543
Joseph Ledo-Massey <104868843+Joeyy16@users.noreply.github.com>
2022-12-30 13:09:33 -0700

    Update main.py

--- Scientific Computing with Python/Polygon Area Calculator/main.py
+++ Scientific Computing with Python/Polygon Area Calculator/main.py
@@ -1,6 +1,13 @@
+"""
+To explain this code, first let's start with the imports. The shape_calculator module is imported, which contains the classes Rectangle and Square.
+
+Then, an instance of Rectangle is created with a width of 5 and a height of 10. The get_area() method of this instance is called and the result is printed. The width of the Rectangle instance is then set to 3 using the set_width() method. The get_perimeter() method is called on the Rectangle instance and the result is printed. Finally, the Rectangle instance is printed using the __repr__() method.
+
+Next, an instance of Square is created with a side length of 9. The get_area() method is called on this instance and the result is printed. The side length of the Square instance is then set to 4 using the set_side() method. The get_diagonal() method is called on the Square instance and the result is printed. Finally, the Square instance is printed using the __repr__() method.
+"""
+
 # This entrypoint file to be used in development. Start by reading README.md
 import shape_calculator
-from unittest import main
 
 
 rect = shape_calculator.Rectangle(5, 10)
@@ -15,6 +22,3 @@ sq.set_side(4)
 print(sq.get_diagonal())
 print(sq)
 
-
-# Run unit tests automatically
-main(module='test_module', exit=False)
