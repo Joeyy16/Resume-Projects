0c6bf2e7be28017939f112e4c679c836a326ed91
Joseph Ledo-Massey <104868843+Joeyy16@users.noreply.github.com>
2022-12-30 15:45:29 -0700

    Update main.py

--- Data Analysis with Python/Sea Level Predictor/main.py
+++ Data Analysis with Python/Sea Level Predictor/main.py
@@ -1,9 +1,12 @@
+"""
+The code above is importing a module called sea_level_predictor and calling a function within it called draw_plot(). Without further context, it is difficult to provide a detailed explanation of what this function does or what it is expected to return. It is possible that the function is used to generate a plot related to sea level data, but this cannot be confirmed without access to the implementation of the function.
+"""
+
 # This entrypoint file to be used in development. Start by reading README.md
 import sea_level_predictor
-from unittest import main
+
 
 # Test your function by calling it here
 sea_level_predictor.draw_plot()
 
-# Run unit tests automatically
-main(module='test_module', exit=False)
+
