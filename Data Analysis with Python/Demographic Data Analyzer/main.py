bbff1f149656510b062893ede44e100b7c2fc365
Joseph Ledo-Massey <104868843+Joeyy16@users.noreply.github.com>
2022-12-30 14:40:01 -0700

    Update main.py

--- Data Analysis with Python/Demographic Data Analyzer/main.py
+++ Data Analysis with Python/Demographic Data Analyzer/main.py
@@ -28,7 +28,7 @@ Then the function might return a dictionary like this:
 
 # This entrypoint file to be used in development. Start by reading README.md
 import demographic_data_analyzer
-from unittest import main
+
 
 # Test your function by calling it here
 demographic_data_analyzer.calculate_demographic_data()
