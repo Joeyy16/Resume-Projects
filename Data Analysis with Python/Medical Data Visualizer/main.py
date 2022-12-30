59dfafcbfbf75613ad6725d1cb9a01586cfc8ba5
Joseph Ledo-Massey <104868843+Joeyy16@users.noreply.github.com>
2022-12-30 15:24:14 -0700

    Update main.py

--- Data Analysis with Python/Medical Data Visualizer/main.py
+++ Data Analysis with Python/Medical Data Visualizer/main.py
@@ -1,10 +1,12 @@
+"""
+This code imports the medical_data_visualizer module and calls two functions from it, draw_cat_plot and draw_heat_map. These functions are likely used to create visualizations using data from a medical dataset. The draw_cat_plot function likely generates a categorical plot, which is a type of chart used to display the distribution of a categorical variable. The draw_heat_map function likely generates a heat map, which is a graphical representation of data where values are represented as colors. Both of these functions are called without any arguments, so they are likely using default parameters or accessing data from a global scope within the medical_data_visualizer module.
+"""
+
 # This entrypoint file to be used in development. Start by reading README.md
 import medical_data_visualizer
-from unittest import main
+
 
 # Test your function by calling it here
 medical_data_visualizer.draw_cat_plot()
 medical_data_visualizer.draw_heat_map()
 
-# Run unit tests automatically
-main(module='test_module', exit=False)
