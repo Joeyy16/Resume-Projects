4685414c9f03163783c35dd8fd1b926da8030968
Joseph Ledo-Massey <104868843+Joeyy16@users.noreply.github.com>
2022-12-30 11:42:35 -0700

    Update main.py

--- Scientific Computing with Python/Time Calculator/main.py
+++ Scientific Computing with Python/Time Calculator/main.py
@@ -1,10 +1,12 @@
 # This entrypoint file to be used in development. Start by reading README.md
+
+"""
+This code imports the add_time() function from the time_calculator module. This allows you to use the add_time() function in your code by calling it as add_time().
+"""
+
 from time_calculator import add_time
-from unittest import main
 
 
-print(add_time("11:06 PM", "2:02"))
+print(add_time("11:06 PM", "2:02")) #calling and printing function
 
 
-# Run unit tests automatically
-main(module='test_module', exit=False)
