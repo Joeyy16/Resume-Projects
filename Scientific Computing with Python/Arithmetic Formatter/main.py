4a5ffce6218615165be8e3aee102496262b1cf6c
Joseph Ledo-Massey <104868843+Joeyy16@users.noreply.github.com>
2022-12-30 11:26:59 -0700

    Update main.py

--- Scientific Computing with Python/Arithmetic Formatter/main.py
+++ Scientific Computing with Python/Arithmetic Formatter/main.py
@@ -1,10 +1,21 @@
 # This entrypoint file to be used in development. Start by reading README.md
+
+"""
+from pytest import main is an import statement in Python that imports the main() function from the pytest module.
+
+The pytest module is a popular testing framework for Python that allows you to write test cases for your code and run them automatically. The main() function is the main entry point for running pytest from the command line. It takes a list of arguments and runs the test cases specified by those arguments.
+
+For example, you could use the following command to run all test cases in the current directory:
+"""
+
+
+"""
+from arithmetic_arranger import arithmetic_arranger is an import statement in Python that imports the arithmetic_arranger() function from the arithmetic_arranger module.
+"""
+
 from arithmetic_arranger import arithmetic_arranger
-from unittest import main
+
+#Calling Function
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])) #Prints called function
 
 
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
-
-
-# Run unit tests automatically
-main(module='test_module', exit=False)
