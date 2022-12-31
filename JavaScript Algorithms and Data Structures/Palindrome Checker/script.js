f7cdc80c992c0f07a802e36e24bf165924cf6a00
Joseph Ledo-Massey <104868843+Joeyy16@users.noreply.github.com>
2022-12-30 19:03:58 -0700

    Update script.js

--- JavaScript Algorithms and Data Structures/Palindrome Checker/script.js
+++ JavaScript Algorithms and Data Structures/Palindrome Checker/script.js
@@ -1,4 +1,17 @@
- function palindrome(str) {
+ /*
+ This code defines a function called palindrome that takes a string as an argument and returns a boolean indicating whether the string is a palindrome or not.
+
+A palindrome is a word or phrase that is spelled the same forwards as it is backwards. For example, the word "racecar" is a palindrome because it reads the same forwards and backwards.
+
+The palindrome function starts by using the replace method to remove all non-letter characters from the input string. It then converts the remaining letters to lowercase and splits the string into an array of individual characters using the split method.
+
+The function then uses a for loop to iterate over the onlyLetters array. For each iteration, it checks if the character at the current index is equal to the character at the opposite end of the array (that is, the character at the index onlyLetters.length - i - 1). If the characters are not equal, the function returns false and exits the loop using the break statement.
+
+If the for loop completes without returning false, the function returns true, indicating that the input string is a palindrome.
+ */
+
+
+function palindrome(str) {
     let onlyLetters = str.replace(/[`~ !@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
     onlyLetters = onlyLetters.toLowerCase().split("");
     for (let i = 0; i < onlyLetters.length - 1 / 2; i++) {
