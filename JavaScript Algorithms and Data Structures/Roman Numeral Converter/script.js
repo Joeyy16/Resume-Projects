b22c4a4faef8997f1f9966cd2f9bb55ee434b530
Joseph Ledo-Massey <104868843+Joeyy16@users.noreply.github.com>
2022-12-30 19:06:01 -0700

    Update script.js

--- JavaScript Algorithms and Data Structures/Roman Numeral Converter/script.js
+++ JavaScript Algorithms and Data Structures/Roman Numeral Converter/script.js
@@ -1,3 +1,15 @@
+/*
+This code defines a function called convertToRoman that takes a number as an argument and returns the Roman numeral representation of that number.
+
+The Roman numeral system uses a set of letters to represent different values. For example, the letter I represents the value 1, the letter V represents the value 5, and the letter X represents the value 10. The Roman numeral system also has rules for constructing larger numbers using these letters. For example, the Roman numeral IV represents the value 4 (5 - 1) and the Roman numeral IX represents the value 9 (10 - 1).
+
+The convertToRoman function starts by defining an array called ref that maps Roman numeral letters to their corresponding values. It then initializes an empty array called res that will be used to store the Roman numeral representation of the input number.
+
+The convertToRoman function then uses the forEach method to iterate over the ref array. For each element in the ref array, the function uses a while loop to add the corresponding Roman numeral letter to the res array as many times as needed to represent the input number. It then subtracts the corresponding value from the input number each time a letter is added to the res array.
+
+Finally, the convertToRoman function returns the Roman numeral representation of the input number by joining the elements of the res array together into a single string using the join method.
+*/
+
 function convertToRoman(num) {
   var ref = [['M', 1000], ['CM', 900], ['D', 500], ['CD', 400], ['C', 100], ['XC', 90], ['L', 50], ['XL', 40], ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]];
   var res = [];
