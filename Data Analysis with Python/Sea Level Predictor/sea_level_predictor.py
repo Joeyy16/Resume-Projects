b3d0cf8b02eade3912ca96f3eb06d60dbd07773f
Joseph Ledo-Massey <104868843+Joeyy16@users.noreply.github.com>
2023-01-02 09:47:25 -0700

    Update sea_level_predictor.py

--- Data Analysis with Python/Sea Level Predictor/sea_level_predictor.py
+++ Data Analysis with Python/Sea Level Predictor/sea_level_predictor.py
@@ -1,65 +1,34 @@
-"""
-These lines of code import the necessary libraries and modules that will be used in the script.
-
-The pandas library is a powerful library for data manipulation and analysis in Python. It is used to import and manipulate the data stored in the .csv file.
-
-The matplotlib.pyplot library is a plotting library used for creating various types of plots in Python. It is used to create a line plot of the sea level data.
-
-The linregress function from the scipy.stats library is used to perform a linear regression on the sea level data and obtain the slope and intercept values for the regression line. This will be used to make predictions about future sea level values.
-"""
-
-import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.stats import linregress
-
-"""
-The code above is reading in a CSV file called 'epa-sea-level.csv' using the read_csv function from the pandas library. This function reads in the data from the file and stores it in a dataframe called 'df'. The data in this file contains information about sea level measurements over time.
-"""
+# Import the necessary libraries and modules that will be used in the script.
+import pandas as pd # The pandas library is a powerful library for data manipulation and analysis in Python. It is used to import and manipulate the data stored in the .csv file.
+import matplotlib.pyplot as plt # The matplotlib.pyplot library is a plotting library used for creating various types of plots in Python. It is used to create a line plot of the sea level data.
+from scipy.stats import linregress # The linregress function from the scipy.stats library is used to perform a linear regression on the sea level data and obtain the slope and intercept values for the regression line. This will be used to make predictions about future sea level values.
 
 def draw_plot():
     # Read data from file
-    df = pd.read_csv('epa-sea-level.csv')
-    
-    """
-    The code above is creating a figure object and an axis object using the plt.subplots() function. The plt.rcParams["figure.figsize"] line sets the size of the figure to be 25 inches wide and 12 inches tall. The ax.set_title(), ax.set_xlabel(), and ax.set_ylabel() lines are setting the title, x-axis label, and y-axis label of the plot, respectively. These labels will be displayed on the plot when it is shown.
-
-    """
+    df = pd.read_csv('epa-sea-level.csv') # The code above is reading in a CSV file called 'epa-sea-level.csv' using the read_csv function from the pandas library. This function reads in the data from the file and stores it in a dataframe called 'df'. The data in this file contains information about sea level measurements over time.
 
     # Create scatter plot
-    fig, ax =plt.subplots() # Create a figure containing a single axes.
-    plt.rcParams["figure.figsize"] = (25, 12)
-    ax.set_title("Rise in Sea Level")
-    ax.set_xlabel('Year')
-    ax.set_ylabel('Sea Level (inches)')
+    fig, ax =plt.subplots() # Creating a figure object and an axis object using the plt.subplots() function.
+    plt.rcParams["figure.figsize"] = (25, 12) # The plt.rcParams["figure.figsize"] line sets the size of the figure to be 25 inches wide and 12 inches tall.
+    ax.set_title("Rise in Sea Level") # Setting the title
+    ax.set_xlabel('Year') # Setting the x-axis label
+    ax.set_ylabel('Sea Level (inches)') # Setting the y-axis label
     
-    """
-    The code above is creating arrays of values from the 'Year' and 'CSIRO Adjusted Sea Level' columns of the df dataframe. It is also creating two new arrays that only contain values from the 'Year' and 'CSIRO Adjusted Sea Level' columns for rows where the 'Year' value is greater than or equal to 2000. It is also finding the minimum value in the 'Year' column and saving it in the variable 'first_year'. These arrays will be used later for plotting and performing a linear regression analysis.
-
-    """
-
-    # MatPlotLib might work unstable with Pandas DataFrame or numpy matrix.
-    # As a work around that, convert data to numpy arrays:
-    year_asarray = df['Year'].values
+    
+    year_asarray = df['Year'].values # Creating an array of values from the 'Year' column of the df dataframe
     y2000_asarray = df.loc[(df.Year >=2000),'Year'].values # Filter out data from 2000
 
     first_year = df['Year'].loc[df['Year'].idxmin()]
 
-    level_asarray = df['CSIRO Adjusted Sea Level'].values
+    level_asarray = df['CSIRO Adjusted Sea Level'].values # Creating an array of values from the 'CSIRO Adjusted Sea Level' column of the df dataframe
     l2000_asarray = df.loc[(df.Year >=2000),'CSIRO Adjusted Sea Level'].values # Filter out data from 2000
     
-    """
-    The code above is creating a scatter plot on the "ax" Axes object. The x-axis of the plot is being set to the values in the "year_asarray" array, and the y-axis is being set to the values in the "level_asarray" array. This will create a scatter plot with one point for each year in "year_asarray" and the corresponding sea level value in "level_asarray".
 
-    """
-
-    # ax.plot(year_asarray, level_asarray, label='linear')
+    # Creating a scatter plot on the "ax" Axes object. The x-axis of the plot is being set to the values in the "year_asarray" array, and the y-axis is being set to the values in the "level_asarray" array. This will create a scatter plot with one point for each year in "year_asarray" and the corresponding sea level value in "level_asarray".
     ax.scatter(year_asarray, level_asarray)
     
-    """
-    The code above is plotting a scatter plot of the 'Year' and 'CSIRO Adjusted Sea Level' columns of the df dataframe on the 'ax' axes object. Then, it is creating an array called 'year_ext_array' that goes from the minimum year in the 'Year' column of df to 2050 in steps of 1. This array will be used to extrapolate the data beyond the range of the original data. The code then performs a linear regression on the 'Year' and 'CSIRO Adjusted Sea Level' columns of the df dataframe and prints the R-squared value of the regression. Finally, it plots the extrapolated data using the 'ax.plot()' function and the 'year_ext_array' array, with the slope and intercept of the linear regression as the parameters.
     
-    """
-    
+    # Creating an array called 'year_ext_array' that goes from the minimum year in the 'Year' column of df to 2050 in steps of 1. This array will be used to extrapolate the data beyond the range of the original data. The code then performs a linear regression on the 'Year' and 'CSIRO Adjusted Sea Level' columns of the df dataframe and prints the R-squared value of the regression. Finally, it plots the extrapolated data using the 'ax.plot()' function and the 'year_ext_array' array, with the slope and intercept of the linear regression as the parameters. 
     # Create first line of best fit
     # Linear regresion curve:
     year_ext_array = range(first_year, 2050,1) # Extrapolated array till 2050
@@ -81,11 +50,7 @@ def draw_plot():
     year_ext_array2 = range(2000, 2050,1) # Extrapolated array from 2000 to 2050
     ax.plot(year_ext_array2, res2.intercept +res2.slope*year_ext_array2, 'r')
 
-    # Add labels and title
 
-    """
-    The code above is defining a function called draw_plot which is used to create a plot showing the rise in sea level over time. It reads in data from a CSV file and creates a scatter plot of the data using ax.scatter(). It then uses the linregress() function from the scipy.stats module to fit a linear regression model to the data and calculates the R-squared value for the model. The function then plots two lines on the same plot using ax.plot(), one representing the linear regression model fit to all the data and another representing the linear regression model fit to data from the year 2000 onwards. Finally, the function saves the plot as an image and returns the Axes object containing the plot.
-    """
-    # Save plot and return data for testing (DO NOT MODIFY)
+    # Save plot and return data for testing
     plt.savefig('sea_level_plot.png')
     return plt.gca()
