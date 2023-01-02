98cdf67c37bb54220807b07d52998994f3e1da33
Joseph Ledo-Massey <104868843+Joeyy16@users.noreply.github.com>
2023-01-01 19:11:40 -0700

    Update main.py

--- Scientific Computing with Python/Budget App/main.py
+++ Scientific Computing with Python/Budget App/main.py
@@ -1,44 +1,26 @@
-# This entrypoint file to be used in development. Start by reading README.md
+# Entrypoint file 
 
-"""
-This code does the following:
-
-Imports the budget module and the create_spend_chart() function from the budget module.
-Creates an object of the Category class from the budget module with the name "Food" and assigns it to the food variable.
-Calls the deposit() method on the food object with the amount 1000 and the description "initial deposit".
-Calls the withdraw() method on the food object with the amount 10.15 and the description "groceries".
-Calls the withdraw() method on the food object with the amount 15.89 and the description "restaurant and more food for dessert".
-Prints the balance of the food object using the get_balance() method.
-Creates an object of the Category class from the budget module with the name "Clothing" and assigns it to the clothing variable.
-Calls the transfer() method on the food object with the amount 50 and the clothing object as arguments.
-Calls the withdraw() method on the clothing object with the amount 25.55.
-Calls the withdraw() method on the clothing object with the amount 100.
-Creates an object of the Category class from the budget module with the name "Auto" and assigns it to the auto variable.
-Calls the deposit() method on the auto object with the amount 1000 and the description "initial deposit".
-Calls the withdraw() method on the auto object with the amount 15.
-Prints the food, clothing, and auto objects using the __str__() method defined in the Category class.
-Calls the `create_spend_
-"""
-
+# Imports the budget module and the create_spend_chart() function from the budget module.
 import budget
 from budget import create_spend_chart
 
 
-food = budget.Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
-clothing = budget.Category("Clothing")
-food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
-auto = budget.Category("Auto")
-auto.deposit(1000, "initial deposit")
-auto.withdraw(15)
+food = budget.Category("Food") # Creates an object of the Category class from the budget module with the name "Food" and assigns it to the food variable.
+food.deposit(1000, "initial deposit") # Calls the deposit() method on the food object with the amount 1000 and the description "initial deposit".
+food.withdraw(10.15, "groceries") # Calls the withdraw() method on the food object with the amount 10.15 and the description "groceries".
+food.withdraw(15.89, "restaurant and more food for dessert") # Calls the withdraw() method on the food object with the amount 15.89 and the description "restaurant and more food for dessert".
+print(food.get_balance()) # Prints the balance of the food object using the get_balance() method.
+clothing = budget.Category("Clothing") # Creates an object of the Category class from the budget module with the name "Clothing" and assigns it to the clothing variable.
+food.transfer(50, clothing) # Calls the transfer() method on the food object with the amount 50 and the clothing object as arguments.
+clothing.withdraw(25.55) # Calls the withdraw() method on the clothing object with the amount 25.55.
+clothing.withdraw(100) # Calls the withdraw() method on the clothing object with the amount 100.
+auto = budget.Category("Auto") # Creates an object of the Category class from the budget module with the name "Auto" and assigns it to the auto variable.
+auto.deposit(1000, "initial deposit") # Calls the deposit() method on the auto object with the amount 1000 and the description "initial deposit".
+auto.withdraw(15) # Calls the withdraw() method on the auto object with the amount 15.
 
+# Prints the food, clothing, and auto objects using the __str__() method defined in the Category class.
 print(food)
 print(clothing)
 
-print(create_spend_chart([food, clothing, auto]))
+print(create_spend_chart([food, clothing, auto])) # Calls the create_spend_chart function
 
